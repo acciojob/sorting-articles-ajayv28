9227fcd13a0eee4cf70dd789bd1459e6a92f4dce
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-10-06 21:29:52 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You',
+    'The Devil Wears Prada',
+    'Pierce the Veil',
+    'Norma Jean',
+    'The Bled',
+    'Say Anything',
+    'The Midway State',
+    'We Came as Romans',
+    'Counterparts',
+    'Oh, Sleeper',
+    'A Skylit Drive',
+    'Anywhere But Here',
+    'An Old Dog'
+];
+
+// Function to sort the bands
+function sortBands(bands) {
+    return bands.sort((a, b) => {
+        const normalize = (str) => str.replace(/^(a|an|the)\s+/i, '').toLowerCase();
+        return normalize(a).localeCompare(normalize(b));
+    });
+}
+
+// Function to render the sorted bands to the DOM
+function renderBands() {
+    const sortedBands = sortBands(bands);
+    const bandList = document.getElementById('band');
+
+    sortedBands.forEach(band => {
+        const li = document.createElement('li');
+        li.textContent = band;
+        bandList.appendChild(li);
+    });
+}
+
+// Initial render
+renderBands();
